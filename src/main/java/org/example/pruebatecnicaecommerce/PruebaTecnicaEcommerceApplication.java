01185d878960a6c1e49f33762d72deec69ea0b1d
elkinssm <elkinsilvamanrique@gmail.com>
2025-09-19 21:34:12 -0500

    chore(init): spring app skeleton from initializer

---
+++ src/main/java/org/example/pruebatecnicaecommerce/PruebaTecnicaEcommerceApplication.java
@@ -0,0 +1,13 @@
+package org.example.pruebatecnicaecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaTecnicaEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pruebatecnicaecommerce/PruebaTecnicaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pruebatecnicaecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
